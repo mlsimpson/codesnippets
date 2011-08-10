f60220393a8aadb18b9d2d8aa1c432ecf30c50f3
Matt Simpson <mr.threv@gmail.com>
2011-08-09 20:22:35 -0400

    Added pandigital.rb

---
+++ pandigital.rb
@@ -0,0 +1,21 @@
+class Integer
+  def pandigital?
+    if self < 0
+      return false
+    end
+    if self.to_s.chars.to_a.include?("0")
+      return false
+    end
+    if self.to_s.length == 9
+      selfchars = self.to_s.chars.to_a.sort
+      if selfchars == (1..9).to_a.to_s.chars.to_a
+        return true
+      else
+        return false
+      end
+    else
+      return false
+    end
+
+  end
+end
