59cd5757765862ea6132c03ee12776660dca6c66
Matt Simpson <mr.threv@gmail.com>
2011-08-13 01:21:10 -0400

    Modified pandigital.rb

--- pandigital.rb
+++ pandigital.rb
@@ -16,6 +16,20 @@ class Integer
     else
       return false
     end
-
+  end
+  def pandigital0?
+    if self < 0
+      return false
+    end
+    if self.to_s.length == 10
+      selfchars = self.to_s.chars.to_a.sort
+      if selfchars == (0..9).to_a.to_s.chars.to_a
+        return true
+      else
+        return false
+      end
+    else
+      return false
+    end
   end
 end
