8083348fa533f33c013b906d676c814abebd5ec9
Matt Simpson <mr.threv@gmail.com>
2012-01-17 12:57:41 -0500

    Modified abmark.sh

--- abmark.sh
+++ abmark.sh
@@ -17,7 +17,7 @@ if [ -n "$1" -a -n "$2" ]
 then
 
 	# first benchmark
-	echo -e  "Benchmarking... 1/3 ($2 HTTP requests)";
+	echo -e  "Benchmarking... 1/4 ($2 HTTP requests)";
 	$ABLOC -n $2 -g /tmp/abmark-data1 $1 > /dev/null
 	echo -e  -e "\nGreat.  Continuing...";
 
@@ -26,7 +26,7 @@ then
 	sleep 5
 
 	# second benchmark
-	echo -e  "Benchmarking... 2/3 ($2 HTTP requests, simulating 25 concurrent users)";
+	echo -e  "Benchmarking... 2/4 ($2 HTTP requests, simulating 25 concurrent users)";
 	$ABLOC -n $2 -c 25 -g /tmp/abmark-data2 $1 > /dev/null
 	echo -e  "\nOkay.  Continuing...";
 
@@ -35,8 +35,18 @@ then
 	sleep 5
 
 	# third benchmark
-	echo -e  "Benchmarking... 3/3 ($2 HTTP requests, simulating 50 concurrent users)";
+	echo -e  "Benchmarking... 3/4 ($2 HTTP requests, simulating 50 concurrent users)";
 	$ABLOC -n $2 -c 50 -g /tmp/abmark-data3 $1 > /dev/null
+
+	echo -e  "\nOne more...";
+
+	# sleep 5 seconds
+	echo -e  "\nsleeping 5 seconds..."
+	sleep 5
+
+	# third benchmark
+	echo -e  "Benchmarking... 4/4 ($2 HTTP requests, simulating 100 concurrent users)";
+	$ABLOC -n $2 -c 100 -g /tmp/abmark-data4 $1 > /dev/null
 
 	echo -e  "\nLooks good. Finished!";
 
@@ -44,6 +54,7 @@ then
 	mv /tmp/abmark-data1 ~/abmark-bench1
 	mv /tmp/abmark-data2 ~/abmark-bench2
 	mv /tmp/abmark-data3 ~/abmark-bench3
+	mv /tmp/abmark-data4 ~/abmark-bench4
 
 else
 
