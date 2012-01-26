54f6e820db9bcfc5e7998d85d4326cdafa0cd599
Matt Simpson <mr.threv@gmail.com>
2012-01-25 19:47:57 -0500

    edited quine.rb

--- quine.rb
+++ quine.rb
@@ -1,7 +1,1 @@
 def s;"def s;;end;puts s()[0,6]+34.chr+s+34.chr+s()[6,s.length-6]";end;puts s()[0,6]+34.chr+s+34.chr+s()[6,s.length-6]
-# puts "\n"
-# puts s
-# puts "\n"
-# puts s()[0,6]
-# puts "\n"
-# puts s[0,6]
