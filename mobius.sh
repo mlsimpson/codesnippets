942c97c546dd1b5d7f64086b1c3e477b80e17fa4
Matt Simpson <mr.threv@gmail.com>
2012-02-16 17:19:16 -0500

    Added mobius.sh

---
+++ mobius.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+
+wget http://archive.wrek.org/main/128kb/Wed1900.mp3
+wget http://archive.wrek.org/main/128kb/Wed1930.mp3
+wget http://archive.wrek.org/main/128kb/Wed2000.mp3
+wget http://archive.wrek.org/main/128kb/Wed2030.mp3
+
+sox Wed1900.mp3 Wed1930.mp3 Wed2000.mp3 Wed2030.mp3 Mobius-`date --date="last wednesday" +"%m-%d-%Y"`.mp3
+
+rm Wed*.mp3
