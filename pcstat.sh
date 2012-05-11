1cb8f409bf8292319ba9b624239e9642963674e0
Matt Simpson <mr.threv@gmail.com>
2012-05-11 00:55:31 -0400

    i rewrote top.  wanna fight about it?

--- pcstat.sh
+++ pcstat.sh
@@ -1,1 +1,6 @@
-ps auxr | awk '{ print "\033[1;31m"$2"\033[0m\t""\033[1;32m"$3"%\033[0m\t"; for (i=11; i<=NF; i++) printf("%s%s", $i, (i==NF) ? "\n" : OFS) }' | awk '{ if ( ( NR % 2 ) == 0 ) { printf("%s\n",$0) } else { printf("%s ",$0) } }' | awk '{ print ( (NR==1) ? "\033[1;34mPID\tLOAD\t COMMAND" : $0 ) }'
+while [ 1 ]
+do
+  ps auxr | awk '{ print "\033[1;31m"$2"\033[0m\t""\033[1;32m"$3"%\033[0m\t"; for (i=11; i<=NF; i++) printf("%s%s", $i, (i==NF) ? "\n" : OFS) }' | awk '{ if ( ( NR % 2 ) == 0 ) { printf("%s\n",$0) } else { printf("%s ",$0) } }' | awk '{ print ( (NR==1) ? "\033[1;34mPID\tLOAD\t COMMAND" : $0 ) }'i | head $LINES
+  sleep 1
+  clear
+done
