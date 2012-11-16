434c193db5e31e11f8ddf206c16710a19091e109
Matt Simpson <mr.threv@gmail.com>
2012-11-16 12:45:42 -0500

    Reset memReport.py o original state & took it out of tmux status

--- memReport.py
+++ memReport.py
@@ -6,7 +6,7 @@ import re
 # Get process info
 ps = subprocess.Popen(['ps', '-caxm', '-orss,comm'],
 stdout=subprocess.PIPE).communicate()[0]
-''' vm = subprocess.Popen(['vm_stat'], stdout=subprocess.PIPE).communicate()[0] '''
+vm = subprocess.Popen(['vm_stat'], stdout=subprocess.PIPE).communicate()[0]
 
 # Iterate processes
 processLines = ps.split('\n')
@@ -21,7 +21,6 @@ for row in range(1,len(processLines)):
     rss = 0 # ignore...
   rssTotal += rss
 
-'''
 # Process vm_stat
 vmLines = vm.split('\n')
 sep = re.compile(':[\s]+')
@@ -30,11 +29,10 @@ for row in range(1,len(vmLines)-2):
   rowText = vmLines[row].strip()
   rowElements = sep.split(rowText)
   vmStats[(rowElements[0])] = int(rowElements[1].strip('\.')) * 4096
-'''
 
-''' print 'Wired Memory:\t\t%d MB' % ( vmStats["Pages wired down"]/1024/1024 )
+print 'Wired Memory:\t\t%d MB' % ( vmStats["Pages wired down"]/1024/1024 )
 print 'Active Memory:\t\t%d MB' % ( vmStats["Pages active"]/1024/1024 )
 print 'Inactive Memory:\t%d MB' % ( vmStats["Pages inactive"]/1024/1024 )
 print 'Free Memory:\t\t%d MB' % ( vmStats["Pages free"]/1024/1024 )
-print 'Real Mem Total (ps):\t%.3f MB' % ( rssTotal/1024/1024) '''
+print 'Real Mem Total (ps):\t%.3f MB' % ( rssTotal/1024/1024)
 print '%dMB' % ( rssTotal/1024/1024)
