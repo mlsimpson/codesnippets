2147da4371a15aeca1df3a4ceca495551857e1ec
Matt Simpson <mr.threv@gmail.com>
2012-11-21 15:19:47 -0500

    Added sshtunnels.sh

---
+++ sshtunnels.sh
@@ -0,0 +1,6 @@
+#!/usr/local/bin/zsh
+
+ssh -f -N -L 6669:irc.choopa.net:6669 threv@bellplantationmarketing.com
+ssh -f -N -L 6666:chat.freenode.net:6666 threv@bellplantationmarketing.com
+ssh -f -N -L 6697:ircs.cmgdigital.com:6697 threv@bellplantationmarketing.com
+ssh -f -N -L 6667:im.bitlbee.org:6667 threv@bellplantationmarketing.com
