608f446735d76bd0534d79d3b01543379a7874e0
Matt Simpson <mr.threv@gmail.com>
2012-12-12 16:07:32 -0500

    ...

--- sshtunnels.sh
+++ sshtunnels.sh
@@ -4,5 +4,3 @@ autossh -M 0 -q -f -N -o "ServerAliveInterval 60" -o "ServerAliveCountMax 3" -L 
 autossh -M 0 -q -f -N -o "ServerAliveInterval 60" -o "ServerAliveCountMax 3" -L 6666:chat.freenode.net:6666 threv@bellplantationmarketing.com
 autossh -M 0 -q -f -N -o "ServerAliveInterval 60" -o "ServerAliveCountMax 3" -L 6697:ircs.cmgdigital.com:6697 threv@bellplantationmarketing.com
 autossh -M 0 -q -f -N -o "ServerAliveInterval 60" -o "ServerAliveCountMax 3" -L 6667:im.bitlbee.org:6667 threv@bellplantationmarketing.com
-
-privoxy /usr/local/etc/privoxy/config
