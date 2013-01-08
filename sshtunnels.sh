22439c5aeec493aa1fcd0e8fca8b56084d55088e
Matt Simpson <mr.threv@gmail.com>
2013-01-08 12:38:53 -0500

    Updated sshtunnels.sh

--- sshtunnels.sh
+++ sshtunnels.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-autossh -M 0 -q -f -N -o "ServerAliveInterval 60" -o "ServerAliveCountMax 3" -L 6669:irc.choopa.net:6669 threv@bellplantationmarketing.com
-autossh -M 0 -q -f -N -o "ServerAliveInterval 60" -o "ServerAliveCountMax 3" -L 6666:chat.freenode.net:6666 threv@bellplantationmarketing.com
-autossh -M 0 -q -f -N -o "ServerAliveInterval 60" -o "ServerAliveCountMax 3" -L 6697:ircs.cmgdigital.com:6697 threv@bellplantationmarketing.com
-autossh -M 0 -q -f -N -o "ServerAliveInterval 60" -o "ServerAliveCountMax 3" -L 6667:im.bitlbee.org:6667 threv@bellplantationmarketing.com
-ssh -D 8080 -f -C -q -N threv@bellplantationmarketing.com
+autossh -M 0 -q -f -N -o "ServerAliveInterval 60" -o "ServerAliveCountMax 3" -L 6669:irc.choopa.net:6669 threv@users.freeshells.org
+autossh -M 0 -q -f -N -o "ServerAliveInterval 60" -o "ServerAliveCountMax 3" -L 6666:chat.freenode.net:6666 threv@users.freeshells.org
+autossh -M 0 -q -f -N -o "ServerAliveInterval 60" -o "ServerAliveCountMax 3" -L 6697:ircs.cmgdigital.com:6697 threv@users.freeshells.org
+autossh -M 0 -q -f -N -o "ServerAliveInterval 60" -o "ServerAliveCountMax 3" -L 6667:im.bitlbee.org:6667 threv@users.freeshells.org
+ssh -D 8080 -f -C -q -N threv@users.freeshells.org
