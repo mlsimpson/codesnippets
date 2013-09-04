cfc83404d1305ed9738c40a3688660b012bcfc74
mlsimpson <mr.threv@gmail.com>
2013-09-04 16:59:55 -0400

    more os agnostic mod to bench.zsh

--- bench.zsh
+++ bench.zsh
@@ -16,7 +16,11 @@ main() {
   echo "zsh:\t$zsh_elapsed_time"
 
   # As above, and also vim wants its stdout to be the terminal.
-  vim_elapsed_time=$( (time /Applications/MacVim.app/Contents/MacOS/Vim +q) 2>&1 >$(tty) )
+  if [[ "$unamestr" == 'Linux' ]]; then
+    vim_elapsed_time=$( (time vim +q) 2>&1 >$(tty) )
+  elif [[ "$unamestr" == 'Darwin' ]]; then
+    vim_elapsed_time=$( (time /Applications/MacVim.app/Contents/MacOS/Vim +q) 2>&1 >$(tty) )
+  fi
   echo "vim:\t$vim_elapsed_time"
 }
 
