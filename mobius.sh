7252d8104a93b5d2c54ff81f8c75783c77aa3f22
mlsimpson <mr.threv@gmail.com>
2013-09-30 13:40:31 -0400

    Added sign.sh

--- mobius.sh
+++ mobius.sh
@@ -7,4 +7,13 @@ wget http://archive.wrek.org/main/128kb/Wed2030.mp3
 
 sox Wed1900.mp3 Wed1930.mp3 Wed2000.mp3 Wed2030.mp3 /media/link/Mobius\ Archives/TheMobius-`date +"%Y-%m-%d"`.mp3
 
+DATE=$(date +"%Y-%m-%d")
+TITLE='The Mobius - $DATE'
+ARTIST='The Mobius'
+ALBUM='The Mobius on WREK Atlanta 91.1 FM'
+YEAR=$(date +"%Y")
+
+mid3v2 -y $YEAR -t $TITLE -a $ARTIST -A $ALBUM --TPE2 $ARTIST /media/link/Mobius\ Archives/TheMobius-`date +"%Y-%m-%d"`.mp3
+mid3v2 --COMM "'\x00\x00\x00'=Every Wednesday from 7 to 9pm EST on WREK Atlanta 91.1 FM, broadcasting from the heart of Georgia Tech.  http://themobius.livejournal.com/" /media/link/Mobius\ Archives/TheMobius-`date +"%Y-%m-%d"`.mp3
+
 rm Wed*.mp3
