3a69ce60c04846676e3c820facbc3853cb380bf5
mlsimpson <mr.threv@gmail.com>
2013-10-28 14:30:52 -0400

    Added mbw.sh; updated mobius.sh

--- mobius.sh
+++ mobius.sh
@@ -5,15 +5,15 @@ wget http://archive.wrek.org/main/128kb/Wed1930.mp3
 wget http://archive.wrek.org/main/128kb/Wed2000.mp3
 wget http://archive.wrek.org/main/128kb/Wed2030.mp3
 
-sox Wed1900.mp3 Wed1930.mp3 Wed2000.mp3 Wed2030.mp3 /media/link/Mobius\ Archives/TheMobius-`date +"%Y-%m-%d"`.mp3
+sox Wed1900.mp3 Wed1930.mp3 Wed2000.mp3 Wed2030.mp3 /media/threv/Lilliput/Mobius\ Archives/TheMobius-`date +"%Y-%m-%d"`.mp3
 
 DATE=$(date +"%Y-%m-%d")
-TITLE='The Mobius - $DATE'
+TITLE='The Mobius - '$DATE
 ARTIST='The Mobius'
 ALBUM='The Mobius on WREK Atlanta 91.1 FM'
 YEAR=$(date +"%Y")
 
-mid3v2 -y $YEAR -t $TITLE -a $ARTIST -A $ALBUM --TPE2 $ARTIST /media/link/Mobius\ Archives/TheMobius-`date +"%Y-%m-%d"`.mp3
-mid3v2 --COMM "'\x00\x00\x00'=Every Wednesday from 7 to 9pm EST on WREK Atlanta 91.1 FM, broadcasting from the heart of Georgia Tech.  http://themobius.livejournal.com/" /media/link/Mobius\ Archives/TheMobius-`date +"%Y-%m-%d"`.mp3
+mid3v2 -y $YEAR -t $TITLE -a $ARTIST -A $ALBUM --TPE2 $ARTIST /media/threv/Lilliput/Mobius\ Archives/TheMobius-`date +"%Y-%m-%d"`.mp3
+mid3v2 --COMM "Every Wednesday from 7 to 9pm EST on WREK Atlanta 91.1 FM, broadcasting from the heart of Georgia Tech.  www.wrek.org || themobius.livejournal.com" /media/threv/Lilliput/Mobius\ Archives/TheMobius-`date +"%Y-%m-%d"`.mp3
 
 rm Wed*.mp3
